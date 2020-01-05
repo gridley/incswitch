e278f75301bad956b3cbda357b84854bcb947919
Gavin Ridley <gavin.keith.ridley@gmail.com>
2020-01-05 14:47:37 -0500

    make faster for most cases

--- rplugin/python3/incswitch.py
+++ rplugin/python3/incswitch.py
@@ -4,18 +4,52 @@ import os
 @pynvim.plugin
 class IncSwitch:
 
+    # Feel free to add more stuff here:
+    headers = {'.h', '.hpp', '.H', '.hh', '.hxx', '.cuh'}
+    sources = {'.c', '.cpp', '.cxx', '.C', '.cu'}
+
     def __init__(self, nvim):
         self.nvim = nvim
 
         # Avoid searching for files if they've already been found once.
         self.cachedLocs = {} 
 
+    def traverse(self, cwd, thisfile):
+        # get file extension
+        justfile = os.path.split(thisfile)[-1]
+        base, extension = os.path.splitext(justfile)
+
+        # see if headers, or sources being searched for
+        if extension in IncSwitch.headers:
+            searchFor = IncSwitch.sources
+        elif extension in IncSwitch.sources:
+            searchFor = IncSwitch.headers
+        else:
+            raise Exception('No recognized header/source correspondence on this file.')
+
+        # traverse current directory and below to find header or source file
+        found = False
+        for root, dirs, files in os.walk(cwd):
+            for name in files:
+                checkFile, checkExt = os.path.splitext(name)
+                if checkExt in searchFor and checkFile == base:
+                    found = True
+                    break
+            else:
+                continue
+            break
+        filepath = os.path.join(root, name)
+
+        # Save location so there's no need to search again.
+        self.cachedLocs[thisfile] = filepath
+
+        if found:
+            return filepath
+        else:
+            return ""
+
     @pynvim.command('IncSwitch')
     def incswitch(self):
-
-        # Feel free to add more stuff here:
-        headers = {'.h', '.hpp', '.H', '.hh', '.hxx', '.cuh'}
-        sources = {'.c', '.cpp', '.cxx', '.C', '.cu'}
 
         # Get current file name
         thisfile = self.nvim.command_output('echo @%').strip()
@@ -27,43 +61,18 @@ class IncSwitch:
 
         # Search for header/source file
         else:
-            # get file extension
-            justfile = os.path.split(thisfile)[-1]
-            base, extension = os.path.splitext(justfile)
+            # Attempt search in current directory
+            filepath = self.traverse(cwd, thisfile)
 
-            # see if headers, or sources being searched for
-            if extension in headers:
-                searchFor = sources
-            elif extension in sources:
-                searchFor = headers
-            else:
-                raise Exception('No recognized header/source correspondence on this file.')
+            if not filepath:
 
-            # start search two directories up from current location. This is
-            # roughly applicable to most code projects where an include directory
-            # is unlikely to be more than two levels away from a src directory.
-            cwd = os.path.join(cwd, '..')
-            cwd = os.path.join(cwd, '..')
+                # Attempt the search two directories up
+                cwd = os.path.join(cwd, '..')
+                cwd = os.path.join(cwd, '..')
+                filepath = self.traverse(cwd, thisfile)
 
-            # traverse current directory and below to find header or source file
-            found = False
-            for root, dirs, files in os.walk(cwd):
-                for name in files:
-                    checkFile, checkExt = os.path.splitext(name)
-                    if checkExt in searchFor and checkFile == base:
-                        found = True
-                        break
-                else:
-                    continue
-                break
-            if not found:
-                raise Exception('Unable to find corresponding source/header file.')
-
-            filepath = os.path.join(root, name)
-
-            # Save location so there's no need to search again.
-            self.cachedLocs[thisfile] = filepath
-
+                if not filepath:
+                    raise Exception('Unable to find corresponding source/header file.')
 
         comm = 'edit ' + filepath
         self.nvim.command(comm)
